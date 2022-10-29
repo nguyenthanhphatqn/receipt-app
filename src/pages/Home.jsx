72f8a2380be76e01c5840cb32367a4d9f9faa07f
Nguyen Thanh Phat <75824684+nguyenthanhphatqn@users.noreply.github.com>
2022-10-29 18:31:45 +0700

    done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
+import { motion } from "framer-motion";
+import React from "react";
+
+function Home() {
+  return (
+    <motion.div>
+      <Veggie />
+      <Popular />
+    </motion.div>
+  );
+}
+
+export default Home;
